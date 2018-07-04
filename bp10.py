093d2ff5204825a73dddc8e364bc6538bae1dc00
oikiou <oikiou@yahoo.co.jp>
2018-07-04 14:49:04 +0900

    修v0.36

--- bp10.py
+++ bp10.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Input, Flatten, Lambda, Conv1D, concatenate
+from keras.layers import Dense, Dropout, Input, Flatten, Lambda, Conv1D, concatenate, Reshape
 from keras.utils import np_utils, plot_model
 from keras.optimizers import RMSprop
 import numpy as np
@@ -54,6 +54,15 @@ train_y_2 = pd.read_csv('temp/train_y_2.csv').values[:, 1:]
 test_x = pd.read_csv('temp/test_x.csv').values[:, 1:]
 test_y_1 = pd.read_csv('temp/test_y.csv').values[:, 1:]
 test_y_2 = pd.read_csv('temp/test_y_2.csv').values[:, 1:]
+print(train_y_2)
+
+train_y_2_1 = train_y_2[:, :2]
+train_y_2_2 = train_y_2[:, 2:4]
+train_y_2_3 = train_y_2[:, 4:]
+
+test_y_2_1 = test_y_2[:, :2]
+test_y_2_2 = test_y_2[:, 2:4]
+test_y_2_3 = test_y_2[:, 4:]
 
 
 # bpnn
@@ -103,38 +112,68 @@ def slice(x, index):
 
 main_input = Input(shape=(3, 8))
 ly_1 = Conv1D(16, 1, activation='relu')(main_input)
-ly_2 = Dense(1, activation='softmax')
+ly_1 = Conv1D(16, 1, activation='relu')(ly_1)
+ly_2 = Dense(2, activation='softmax')
 
 ro = []
+ri = []
 for n in range(3):
-	r = Lambda(slice, output_shape=(1, 16), arguments={'index': n})(ly_1)
+	r = Lambda(slice, arguments={'index': n})(ly_1)
+	ri.append(r)
 	ro.append(ly_2(r))
 
+ly_3_1 = ly_2(ri[0])
+ly_3_2 = ly_2(ri[1])
+ly_3_3 = ly_2(ri[2])
+
+
 ly_3 = concatenate([r for r in ro])
+ly_4 = concatenate([r for r in ri])
 
-model = Model(inputs=main_input, outputs=ly_3)
+input_2 = Input(shape=(12,))
+ly_5 = Dense(12, activation='relu')(ly_4)
+ly_6 = concatenate([input_2, ly_5])
+ly_7 = Dense(60, activation='relu')(ly_6)
+ly_8 = Dense(11, activation='softmax')(ly_7)
 
 
+# model = Model(inputs=[main_input, input_2], outputs=[ly_3, ly_8])
+# model = Model(inputs=main_input, outputs=ly_3)
+model = Model(inputs=main_input, outputs=[ly_3_1, ly_3_2, ly_3_3])
 
 
 print(model.summary())
 
 
-model.compile(loss='mse', optimizer='sgd', metrics=['accuracy'])
+# model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.001), loss_weights=[0.5, 0.5], metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.001), metrics=['accuracy'])
 
-model.fit(x=train_x_1, y=train_y_2, epochs=10, shuffle=True, verbose=1)
+# model.fit(x=[train_x_1, train_x_2], y=[train_y_2, train_y_1], epochs=10, shuffle=True, verbose=1)
+# model.fit(x=train_x_1, y=train_y_2, epochs=10, shuffle=True, verbose=1)
+model.fit(x=train_x_1, y=[train_y_2_1, train_y_2_2, train_y_2_3], epochs=5, shuffle=True, verbose=1)
 
-loss, accuracy = model.evaluate(x=test_x_1, y=test_y_2)
-print(loss, accuracy)
+# loss = model.evaluate(x=[test_x_1, test_x_2], y=[test_y_2, test_y_1])
+# loss = model.evaluate(x=test_x_1, y=test_y_2)
+loss = model.evaluate(x=test_x_1, y=[test_y_2_1, test_y_2_2, test_y_2_3])
+print(loss)
+
 #
 # w = model.get_weights()
 # # print(w)
-pred = model.predict(x=test_x_1)
-pd.DataFrame(pred).to_csv('temp/pred.csv')
 
-pred2 = model.predict(x=train_x_1)
+# pred0, pred1 = model.predict(x=[test_x_1, test_x_2])
+pred1, pred2, pred3 = model.predict(x=test_x_1)
+pd.DataFrame(pred1).to_csv('temp/pred1.csv')
 pd.DataFrame(pred2).to_csv('temp/pred2.csv')
+pd.DataFrame(pred3).to_csv('temp/pred3.csv')
 
+# print(pred0, pred1)
+# pd.DataFrame(pred0).to_csv('temp/pred0.csv')
+# pd.DataFrame(pred1).to_csv('temp/pred1.csv')
+#
+# pred2, pred3 = model.predict(x=[train_x_1, train_x_2])
+# pd.DataFrame(pred2).to_csv('temp/pred2.csv')
+# pd.DataFrame(pred3).to_csv('temp/pred3.csv')
 
 
 
